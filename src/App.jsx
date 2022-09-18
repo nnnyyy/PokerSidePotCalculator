b4eac686a3a16f89296beca83a219832cdc19790
nnnyyy <nnnyyy@nate.com>
2022-09-19 01:37:41 +0900

    부트스트랩 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React, { useRef, useState } from "react";
 import { observer } from "mobx-react";
 import holdemStore, { STATE_FOLD, STATE_PLAY } from "./Stores";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = observer(() => {
     return (
         <div className="App">
-            <div>
-                <table>
-                    <thead>
+            <div className="p-3">
+                <div className="h2">사이드팟 계산</div>
+                <button type="button" className="btn btn-outline-primary mt-3" onClick={(e) => holdemStore.players.forEach((p) => (p.rank = 4))}>
+                    순위 리셋
+                </button>
+                <table className="table table-bordered table-striped mt-3">
+                    <thead className="table-dark">
                         <tr>
-                            <td>이름</td>
-                            <td>상태</td>
-                            <td>올인 금액</td>
-                            <td>순위</td>
-                            <td>
-                                순위 설정 <button onClick={(e) => holdemStore.players.forEach((p) => (p.rank = 4))}>reset</button>
-                            </td>
+                            <td style={{ width: "100px" }}>이름</td>
+                            <td style={{ width: "80px" }}>상태</td>
+                            <td style={{ width: "140px" }}>올인 금액</td>
+                            <td style={{ width: "80px" }}>순위</td>
+                            <td style={{ width: "160px" }}>순위 설정</td>
                             <td>결과</td>
                         </tr>
                     </thead>
@@ -30,20 +33,20 @@ const App = observer(() => {
                                         </div>
                                     </td>
                                     <td>
-                                        <input disabled={p.state == STATE_FOLD} value={p.allinCash} onChange={(e) => holdemStore.setCash(idx, e.target.value)} />
+                                        <input style={{ width: "100px" }} disabled={p.state == STATE_FOLD} value={p.allinCash} onChange={(e) => holdemStore.setCash(idx, e.target.value)} />
                                     </td>
                                     <td>{p.state == STATE_FOLD ? "-" : p.rank}</td>
                                     <td>
-                                        <button disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 1)}>
+                                        <button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 1)}>
                                             1
                                         </button>
-                                        <button disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 2)}>
+                                        <button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 2)}>
                                             2
                                         </button>
-                                        <button disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 3)}>
+                                        <button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 3)}>
                                             3
                                         </button>
-                                        <button disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 4)}>
+                                        <button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 4)}>
                                             4
                                         </button>
                                     </td>
