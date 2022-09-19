207de011d34d234b65aff5c4269eb6436fa6f777
nnnyyy <nnnyyy@nate.com>
2022-09-19 18:22:21 +0900

    계산기 만드는 중

--- src/UICalc.jsx
+++ src/UICalc.jsx
@@ -7,7 +7,17 @@ export const UICalc = observer(({ idx }) => {
 	return (
 		<>
 			<div className="calc-absolute">
-				<div className="content p-2">계산기 {holdemStore.players[idx].allinCash}</div>
+				<div className="content p-2 position-relative">
+					<button className="btn btn-outline-success position-absolute btn-calc num-1">1</button>
+					<button className="btn btn-outline-success position-absolute btn-calc num-2">2</button>
+					<button className="btn btn-outline-success position-absolute btn-calc num-3">3</button>
+					<button className="btn btn-outline-success position-absolute btn-calc num-4">4</button>
+					<button className="btn btn-outline-success position-absolute btn-calc num-5">5</button>
+					<button className="btn btn-outline-success position-absolute btn-calc num-6">6</button>
+					<button className="btn btn-outline-success position-absolute btn-calc num-7">7</button>
+					<button className="btn btn-outline-success position-absolute btn-calc num-8">8</button>
+					<button className="btn btn-outline-success position-absolute btn-calc num-9">9</button>
+				</div>
 			</div>
 		</>
 	);
