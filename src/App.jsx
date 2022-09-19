54594b1b8a6494268f825b22d0b7086b6f1209a2
nnnyyy <nnnyyy@nate.com>
2022-09-19 15:08:52 +0900

    코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -4,69 +4,69 @@ import holdemStore, { STATE_FOLD, STATE_PLAY } from "./Stores";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = observer(() => {
-    return (
-        <div className="App">
-            <div className="p-3">
-                <div className="h2">사이드팟 계산</div>
-                <button type="button" className="btn btn-outline-primary mt-3" onClick={(e) => holdemStore.players.forEach((p) => (p.rank = 4))}>
-                    순위 리셋
-                </button>
-                <table className="table table-bordered table-striped mt-3">
-                    <thead className="table-dark">
-                        <tr>
-                            <td style={{ width: "100px" }}>이름</td>
-                            <td style={{ width: "80px" }}>상태</td>
-                            <td style={{ width: "140px" }}>올인 금액</td>
-                            <td style={{ width: "80px" }}>순위</td>
-                            <td style={{ width: "160px" }}>순위 설정</td>
-                            <td>결과</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {holdemStore.players.map((p, idx) => {
-                            return (
-                                <tr key={idx}>
-                                    <td>{p.name}</td>
-                                    <td>
-                                        <div style={{ cursor: "pointer" }} onClick={(e) => holdemStore.setState(idx, p.state == STATE_PLAY ? STATE_FOLD : STATE_PLAY)}>
-                                            {p.state == STATE_PLAY ? "플레이" : "폴드"}
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <input style={{ width: "100px" }} disabled={p.state == STATE_FOLD} value={p.allinCash} onChange={(e) => holdemStore.setCash(idx, e.target.value)} />
-                                    </td>
-                                    <td>{p.state == STATE_FOLD ? "-" : p.rank}</td>
-                                    <td>
-                                        <button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 1)}>
-                                            1
-                                        </button>
-                                        <button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 2)}>
-                                            2
-                                        </button>
-                                        <button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 3)}>
-                                            3
-                                        </button>
-                                        <button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 4)}>
-                                            4
-                                        </button>
-                                    </td>
-                                    <td>
-                                        {p.totalEarn > 0 ? "+" : ""}
-                                        {p.totalEarn} ({" "}
-                                        <span style={{ color: p.totalEarn - p.allinCash > 0 ? "green" : "inherit" }}>
-                                            {p.totalEarn - p.allinCash > 0 ? "+" : ""}
-                                            {p.totalEarn - p.allinCash}{" "}
-                                        </span>
-                                        )
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+	return (
+		<div className="App">
+			<div className="p-3">
+				<div className="h2">사이드팟 계산</div>
+				<button type="button" className="btn btn-outline-primary mt-3" onClick={(e) => holdemStore.players.forEach((p) => (p.rank = 4))}>
+					순위 리셋
+				</button>
+				<table className="table table-bordered table-striped mt-3">
+					<thead className="table-dark">
+						<tr>
+							<td style={{ width: "100px" }}>이름</td>
+							<td style={{ width: "80px" }}>상태</td>
+							<td style={{ width: "140px" }}>올인 금액</td>
+							<td style={{ width: "80px" }}>순위</td>
+							<td style={{ width: "160px" }}>순위 설정</td>
+							<td>결과</td>
+						</tr>
+					</thead>
+					<tbody>
+						{holdemStore.players.map((p, idx) => {
+							return (
+								<tr key={idx}>
+									<td>{p.name}</td>
+									<td>
+										<div style={{ cursor: "pointer" }} onClick={(e) => holdemStore.setState(idx, p.state == STATE_PLAY ? STATE_FOLD : STATE_PLAY)}>
+											{p.state == STATE_PLAY ? "플레이" : "폴드"}
+										</div>
+									</td>
+									<td>
+										<input style={{ width: "100px" }} disabled={p.state == STATE_FOLD} value={p.allinCash} onChange={(e) => holdemStore.setCash(idx, e.target.value)} />
+									</td>
+									<td>{p.state == STATE_FOLD ? "-" : p.rank}</td>
+									<td>
+										<button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 1)}>
+											1
+										</button>
+										<button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 2)}>
+											2
+										</button>
+										<button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 3)}>
+											3
+										</button>
+										<button className="me-2" disabled={p.state == STATE_FOLD} onClick={(e) => holdemStore.setRank(idx, 4)}>
+											4
+										</button>
+									</td>
+									<td>
+										{p.totalEarn > 0 ? "+" : ""}
+										{p.totalEarn} (
+										<span style={{ color: p.totalEarn - p.allinCash > 0 ? "green" : "inherit" }}>
+											{p.totalEarn - p.allinCash > 0 ? "+" : ""}
+											{p.totalEarn - p.allinCash}
+										</span>
+										)
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 });
 
 export default App;
--- src/Stores.jsx
+++ src/Stores.jsx
@@ -4,78 +4,76 @@ export const STATE_FOLD = 0;
 export const STATE_PLAY = 1;
 
 export class HoldemStore {
-    players = [
-        { name: "A", allinCash: 1200, state: STATE_PLAY, rank: 4, cacheCash: 0, totalEarn: 0 },
-        { name: "B", allinCash: 0, state: STATE_FOLD, rank: 4, cacheCash: 0, totalEarn: 0 },
-        { name: "C", allinCash: 0, state: STATE_FOLD, rank: 1, cacheCash: 0, totalEarn: 0 },
-        { name: "D", allinCash: 0, state: STATE_FOLD, rank: 7, cacheCash: 0, totalEarn: 0 },
-        { name: "E", allinCash: 500, state: STATE_PLAY, rank: 1, cacheCash: 0, totalEarn: 0 },
-        { name: "F", allinCash: 1500, state: STATE_PLAY, rank: 1, cacheCash: 0, totalEarn: 0 },
-        { name: "G", allinCash: 700, state: STATE_PLAY, rank: 2, cacheCash: 0, totalEarn: 0 },
-        { name: "H", allinCash: 0, state: STATE_FOLD, rank: 1, cacheCash: 0, totalEarn: 0 },
-        { name: "I", allinCash: 2200, state: STATE_PLAY, rank: 3, cacheCash: 0, totalEarn: 0 },
-        { name: "J", allinCash: 0, state: STATE_FOLD, rank: 8, cacheCash: 0, totalEarn: 0 },
-    ];
+	players = [
+		{ name: "A", allinCash: 1200, state: STATE_PLAY, rank: 4, cacheCash: 0, totalEarn: 0 },
+		{ name: "B", allinCash: 0, state: STATE_FOLD, rank: 4, cacheCash: 0, totalEarn: 0 },
+		{ name: "C", allinCash: 0, state: STATE_FOLD, rank: 1, cacheCash: 0, totalEarn: 0 },
+		{ name: "D", allinCash: 0, state: STATE_FOLD, rank: 7, cacheCash: 0, totalEarn: 0 },
+		{ name: "E", allinCash: 500, state: STATE_PLAY, rank: 1, cacheCash: 0, totalEarn: 0 },
+		{ name: "F", allinCash: 1500, state: STATE_PLAY, rank: 1, cacheCash: 0, totalEarn: 0 },
+		{ name: "G", allinCash: 700, state: STATE_PLAY, rank: 2, cacheCash: 0, totalEarn: 0 },
+		{ name: "H", allinCash: 0, state: STATE_FOLD, rank: 1, cacheCash: 0, totalEarn: 0 },
+		{ name: "I", allinCash: 2200, state: STATE_PLAY, rank: 3, cacheCash: 0, totalEarn: 0 },
+		{ name: "J", allinCash: 0, state: STATE_FOLD, rank: 8, cacheCash: 0, totalEarn: 0 },
+	];
 
-    constructor() {
-        makeAutoObservable(this);
-        this.recalc();
-    }
+	constructor() {
+		makeAutoObservable(this);
+		this.recalc();
+	}
 
-    setCash(idx, allinCash) {
-        this.players[idx].allinCash = Number(allinCash);
-        this.recalc();
-    }
+	setCash(idx, allinCash) {
+		this.players[idx].allinCash = Number(allinCash);
+		this.recalc();
+	}
 
-    setState(idx, state) {
-        this.players[idx].state = state;
-        if (state == STATE_FOLD) {
-            this.players[idx].allinCash = 0;
-            this.players[idx].cacheCash = 0;
-            this.players[idx].rank = 10;
-        } else {
-            this.players[idx].allinCash = 100;
-            this.players[idx].cacheCash = 0;
-        }
-        this.recalc();
-    }
+	setState(idx, state) {
+		this.players[idx].state = state;
+		if (state == STATE_FOLD) {
+			this.players[idx].allinCash = 0;
+			this.players[idx].cacheCash = 0;
+			this.players[idx].rank = 10;
+		} else {
+			this.players[idx].allinCash = 100;
+			this.players[idx].cacheCash = 0;
+		}
+		this.recalc();
+	}
 
-    setRank(idx, rank) {
-        this.players[idx].rank = rank;
-        this.recalc();
-    }
+	setRank(idx, rank) {
+		this.players[idx].rank = rank;
+		this.recalc();
+	}
 
-    recalc() {
-        let playingUsers = this.players.filter((p) => p.state == STATE_PLAY);
-        this.players.forEach((p) => {
-            p.cacheCash = p.allinCash;
-            p.totalEarn = 0;
-        });
+	recalc() {
+		let playingUsers = this.players.filter((p) => p.state == STATE_PLAY);
+		this.players.forEach((p) => {
+			p.cacheCash = p.allinCash;
+			p.totalEarn = 0;
+		});
 
-        const pots = [];
-        while (playingUsers.filter((p) => p.cacheCash > 0).length > 0) {
-            const playables = playingUsers.filter((p) => p.cacheCash > 0);
-            playables.sort((a, b) => (Number(a.cacheCash) > Number(b.cacheCash) ? 1 : -1));
-            const minBet = playables[0].cacheCash;
-            const pot = minBet * playables.length;
-            const playerForPot = playables;
-            pots.push({ pot, playerForPot });
-            playables.forEach((p) => (p.cacheCash -= minBet));
-        }
+		const pots = [];
+		while (playingUsers.filter((p) => p.cacheCash > 0).length > 0) {
+			const playables = playingUsers.filter((p) => p.cacheCash > 0);
+			playables.sort((a, b) => (Number(a.cacheCash) > Number(b.cacheCash) ? 1 : -1));
+			const minBet = playables[0].cacheCash;
+			const pot = minBet * playables.length;
+			const playerForPot = playables;
+			pots.push({ pot, playerForPot });
+			playables.forEach((p) => (p.cacheCash -= minBet));
+		}
 
-        console.log(pots);
-
-        let round = 1;
-        for (const potInfo of pots) {
-            const { playerForPot, pot } = potInfo;
-            playerForPot.sort((a, b) => (a.rank > b.rank ? 1 : -1));
-            const winRank = playerForPot[0].rank;
-            const winners = playerForPot.filter((p) => p.rank <= winRank);
-            const winCash = pot / winners.length;
-            winners.forEach((winner) => (winner.totalEarn += winCash));
-            round++;
-        }
-    }
+		let round = 1;
+		for (const potInfo of pots) {
+			const { playerForPot, pot } = potInfo;
+			playerForPot.sort((a, b) => (a.rank > b.rank ? 1 : -1));
+			const winRank = playerForPot[0].rank;
+			const winners = playerForPot.filter((p) => p.rank <= winRank);
+			const winCash = pot / winners.length;
+			winners.forEach((winner) => (winner.totalEarn += winCash));
+			round++;
+		}
+	}
 }
 
 export default new HoldemStore();
